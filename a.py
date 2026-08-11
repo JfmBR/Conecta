5b781315b2acf763052e2e3895fc177b3898af4d
João Francisco <joaomalaquias2006@gmail.com>
2026-08-11 10:04:29 -0400

    a

--- a.py
+++ a.py
@@ -6,3 +6,8 @@ dinheiro = 2000.503
 print(type(nome))
 print(f"Olá meu nome é {nome} e tenho {idade} anos")
 print(f"tenho R$ {dinheiro:.2f}")
+print(nome.strip())
+print(nome.upper())
+print(nome.lower())
+meses = "Janeiro, fevereiro, Março, Abril"
+print (meses.split(","))
