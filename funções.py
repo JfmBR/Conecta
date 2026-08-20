c482b29182f074d5b0a041ccd60fb8512710e724
João Francisco <joaomalaquias2006@gmail.com>
2026-08-20 08:14:10 -0400

    adiciona novas funções e lista de tarefas

--- funções.py
+++ funções.py
@@ -29,6 +29,7 @@ def saudacao(nome, saudacao = "Olá"):
 saudacao("joao")
 """
 
+"""
 def calcular_frete(peso,distancia_km,expresso = False):
     frete = (peso * 2) + (distancia_km * 0.5)
     if expresso == True:
@@ -37,4 +38,72 @@ def calcular_frete(peso,distancia_km,expresso = False):
     else:
         print(f"valor do frete: {frete}")
     
-calcular_frete(50,100, True)
+calcular_frete(50,100, True)"""
+
+"""
+def calcular_total(valores:list):
+    if valores == []:
+        return None
+    total = sum(valores)
+    return total
+
+print(calcular_total([200,20,50,40,50]))"""
+
+"""
+def triangulo_validor(a,b,c):
+    lado = sorted([a,b,c])
+    if lado[0] + lado[1] <= lado[2]:
+        return "Não é um triangulo"
+    elif a == b == c:
+        return "Equilatero"
+    elif a == b or b == c or c == a:
+        return "Isoceles"
+print(triangulo_validor(1,2,3))
+print(triangulo_validor(1,8,8))
+print(triangulo_validor(3,3,3))"""
+
+"""
+contador_global = 0
+
+def incremento():
+    global contador_global
+    contador_global += 1
+    resultado_local = contador_global * 2
+    return resultado_local
+
+print (incremento())
+print (incremento())"""
+
+"""
+def pedir_numero(mensagem, minimo = None, maximo = None, tipo = float):
+    while True:
+        try:
+            valor = tipo(input(mensagem))
+            if minimo is not None and valor < minimo:
+                print (f"Valor deve ser >= {minimo}.");continue
+            elif maximo is not None and valor > maximo:
+                print(f"valor deve ser <= {maximo}.");continue
+            return valor
+        except ValueError:
+            print("Entrada invalida. Digite um numero valido")
+
+preco = pedir_numero("Preco (R$):", minimo = 0.01)
+print (preco)
+"""
+"""
+def dividir_seguro(a,b):
+    if b == 0:
+        return None
+    else:
+        resultado = a / b
+        return resultado
+"""
+
+def estatisticas(lista:list):
+    soma = sum(lista)
+    media = sum(lista) / len(lista)
+    maior = max(lista)
+    minimo = min(lista)
+    return soma, media, maior, minimo
+resultado = estatisticas([32,50,80,50])
+print (resultado)
---
+++ listas.py
@@ -0,0 +1,21 @@
+""""
+tarefas = ["ler e-mails", "Gerar relatorios"]
+tarefas.append("Enviar relatorios")  #adiciona 1 item
+tarefas.extend(["Arquivar", "Notificar"]) #adiciona varios
+tarefas.insert(0, "Fazer backup") #insere em posição
+tarefas.remove("ler e-mails") #remove pelo item
+print(tarefas)
+item = tarefas.pop(2) #remove desejado e retorna
+print(item)
+print(tarefas)"""
+
+"""
+meses = ["jan", "fev", "mar", "abr", "mai", "jun"]
+#lista[inicio,fim,passos] -- fim não é incluso
+primeiro_trimestre = meses[0:3] #[jan,fev,mar]
+ultimos_dois = meses[-2:] #[mai, jun]
+invertida = meses[: :-1] """
+
+
+
+
